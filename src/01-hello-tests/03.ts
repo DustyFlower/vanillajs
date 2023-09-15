acc5356dde2d83c8e8960130aa5a3f8243865336
DustyFlower <lukashevanatalia1992@gmail.com>
2023-09-15 17:09:26 +0300

    Functions + tests final

--- src/01-hello-tests/03.ts
+++ src/01-hello-tests/03.ts
@@ -1,4 +1,5 @@
 import {StudentType} from './02';
+import {CityType, GovernmentBuildingsType, HouseType} from './02_02';
 
 export const sum = (a: number, b: number) => {
     return a + b;
@@ -28,4 +29,25 @@ export function makeStudentActive(s: StudentType) {
 
 export const doesStudentLiveIn = (s: StudentType, cityName: string) => {
     return s.address.city.title === cityName;
+}
+
+export const addMoneyToBudget = (building: GovernmentBuildingsType, budget: number) => {
+    building.budget += budget;
+}
+
+export const repairHouse = (houseType: HouseType) => {
+    houseType.repaired = true;
+}
+
+export const toFireStaff = (building: GovernmentBuildingsType, staffCountToFire: number) => {
+    building.staffCount -= staffCountToFire;
+}
+
+export const toHireStaff = (building: GovernmentBuildingsType, staffCountToHire: number) => {
+    building.staffCount += staffCountToHire;
+}
+
+export function createMessage(props: CityType) {
+ //   return 'Hello ' + props.title + ' citizens. I want you to be happy. All ' + props.citizensNumber + ' men'
+    return `Hello ${props.title} citizens. I want you to be happy. All ${props.citizensNumber} men`
 }
